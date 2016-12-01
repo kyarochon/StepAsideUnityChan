84b486278209d9778cc2686ae327a07e38223ca1
Kiyotaka Saitou <kiyotaka-saitou@united.jp>
2016-12-01 09:25:04 +0900

    ゲームメイン部分作成
    ゲームの仕組みを実装し、アイテムの動的生成・削除を追加しました。

---
+++ Assets/StepAsideUnityChan/Script/ItemDestroyer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemDestroyer : MonoBehaviour {
+	public GameObject unitychan;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (unitychan.GetComponent<Transform> ().position.z - 10 > this.transform.position.z) {
+			Destroy (this.gameObject);
+		}
+	}
+}
